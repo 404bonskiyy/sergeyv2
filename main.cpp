228714a2526f0d0cc835179915702a0a81bf0fb7
eerytok <stepanukkirilo96@gmail.com>
2026-09-20 12:40:18 +0300

    Halo

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    double a, b;
+    char operation;
+
+    std::cout << "Enter first number: ";
+    std::cin >> a;
+
+    std::cout << "Enter operation (+, -, *, /): ";
+    std::cin >> operation;
+
+    std::cout << "Enter second number: ";
+    std::cin >> b;
+
+    if (operation == '+') {
+        std::cout << "Result: " << a + b << std::endl;
+    }
+    else if (operation == '-') {
+        std::cout << "Result: " << a - b << std::endl;
+    }
+    else if (operation == '*') {
+        std::cout << "Result: " << a * b << std::endl;
+    }
+    else if (operation == '/') {
+        if (b != 0) {
+            std::cout << "Result: " << a / b << std::endl;
+        }
+        else {
+            std::cout << "Error: division by zero!" << std::endl;
+        }
+    }
+    else {
+        std::cout << "Error: invalid operation!" << std::endl;
+    }
+
+    return 0;
+}
